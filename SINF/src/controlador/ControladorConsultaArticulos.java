37273a89486785d655ef5a5040d58ae610b64ca8
DAMR1990 <48568826+DAMR1990@users.noreply.github.com>
2019-05-06 09:48:29 -0500

    Tabla corregida
    se corrigio una desactivaciòn en la actualizaciòn de la tabla

--- SINF/src/controlador/ControladorConsultaArticulos.java
+++ SINF/src/controlador/ControladorConsultaArticulos.java
@@ -483,7 +483,7 @@ public class ControladorConsultaArticulos implements ActionListener,FocusListene
 	 * llena los campos en base al renglon seleccionado actualmente
 	 */
 	public void valueChanged(ListSelectionEvent t) {
-		//modeloConsultas.llenaCampos(vistaConsultas);
+		modeloConsultas.llenaCampos(vistaConsultas);
 	}//fin valueChanged
 	
 	/**
@@ -491,8 +491,8 @@ public class ControladorConsultaArticulos implements ActionListener,FocusListene
 	 * lo desactive para evitar bucle infinito de actualizaciones
 	 */
 	public void tableChanged(TableModelEvent t) {
-		//((Component)t.getSource()).setBackground(Color.LIGHT_GRAY);
-		//modeloConsultas.limpiaCampos(vistaConsultas);
+		((Component)t.getSource()).setBackground(Color.LIGHT_GRAY);
+		modeloConsultas.limpiaCampos(vistaConsultas);
 	}//fin table changed
 	
 	
